8c6887fa4fdc9dbf0df7b8b5b4c5e0771ce11f0c
dishonestea <vishay8@gmail.com>
2020-07-15 00:28:25 -0500

    Updates

---
+++ precache-manifest.2b7c92c47019b90455c9ac07662fd432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca76402bc6cadd9bf8eafc11b3e0760b",
+    "url": "/Quote-Machine/index.html"
+  },
+  {
+    "revision": "da66596988962f405934",
+    "url": "/Quote-Machine/static/css/main.03cd5dc5.chunk.css"
+  },
+  {
+    "revision": "31b15b34f1927a8557f3",
+    "url": "/Quote-Machine/static/js/2.1311147f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Machine/static/js/2.1311147f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da66596988962f405934",
+    "url": "/Quote-Machine/static/js/main.bc9597f2.chunk.js"
+  },
+  {
+    "revision": "40ac3dc9f54c465276ed",
+    "url": "/Quote-Machine/static/js/runtime-main.60a026f4.js"
+  }
+]);
